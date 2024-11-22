073ffbad014c16e876e666aca60d2a59209f5314
be3_ockapa <egor.makeev2017@gmail.com>
2024-11-22 10:47:37 +0500

    Крч, да/нет воспринимает, но сообщения всё также не отправляется

--- import_telebot.py
+++ import_telebot.py
@@ -3,34 +3,40 @@ token = '7792408011:AAEZKlxr5KchQPOhR5O8VPTC7M0prW2uhV4';
 bot = telebot.TeleBot(token);  
 
 #7729336143:AAFs3fKMgC7PDEhmwSm1-ZChBdg9alfsHcs -1    7792408011:AAEZKlxr5KchQPOhR5O8VPTC7M0prW2uhV4 -2
-
+user_message = ""
 #Приветсвие
+
 @bot.message_handler(commands=['start'])
 def welcome(message):
 
     msg = bot.send_message(message.chat.id, "Привет, я бот, напиши /text, чтобы ввести своё сообщение")
     bot.register_next_step_handler(msg, callback=hand)
 
-@bot.message_handler(commands=['text'])
+@bot.message_handler(commands=['text'], func=lambda message: True)
 def hand(message):                                                                                          #Ввод сообщения
 
     msg = bot.send_message(message.chat.id, "Введи своё сообщение")
+    global user_message
+    user_message = message.text;
     bot.register_next_step_handler(msg, callback=messg)
         
 def messg(message):                                                                                          #Отправка
 
     bot.send_message(message.chat.id, "Стоит ли отправлять Твоё сообщение?")
+    msg = bot.send_message(message.chat.id, "Напиши Да/Нет.")
+    bot.register_next_step_handler(msg, callback=yn)
 
 def yn(message):
+    global user_message
     owner_id = 5426110205
-    if message.text == 'Хз':
-        bot.send_message(message.chat.id, "Вот и я хз, отправлять, или нет")
-    elif message.text =="Да":	
+    if message.text =="Да":	
         bot.send_message(message.chat.id, "Хорошо! Сообщение отправлено!")
-        bot.forward_message(message.chat.id, message.text, owner_id)
-    else:
+        bot.forward_message({user_message}, message.chat.id, owner_id)
+    if message.text == 'Хз':
+        bot.send_message(message.chat.id, "Я хз, отправлять, или нет")
+    if message.text=="Нет":
         msg = bot.send_message(message.chat.id, "Плохо! Чтож, давай по новой.")
-        return(bot.register_next_step_handler(msg, callback=hand))       
+        return(bot.register_next_step_handler(msg, callback=hand)) 
     
 
 bot.polling(none_stop=True, interval=0) 
